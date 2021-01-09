087c88bac8c0bcb33e2ad8fdf77d493107596ee0
Sharath <sharath@fashtag.info>
2021-01-09 20:10:34 +0530

    Initial commit

---
+++ exercise1.py
@@ -0,0 +1,11 @@
+print("hello world")
+print("hello again")
+print("this is fun")
+print("its my birthday")
+
+print("hens",25+30/6)
+print(2+3+8+9-6-8>10+9+8-9-7)
+print(3+2>5-7)
+
+
+
---
+++ exercise2.py
@@ -0,0 +1,68 @@
+cars = 100
+drivers = 30
+passengers = 90
+empty_cars = cars-drivers
+avg_passengers = passengers/drivers
+print("no of cars", cars)
+print("no of empty cars", empty_cars)
+
+my_name = 'Puneeth'
+my_age = 25
+my_height = 6
+my_weight = 108
+my_eyes = 'black'
+print(f"lets talk about {my_name}.")
+print(f"my age is {my_age}")
+print(f"my height is {my_height}ft")
+
+types_of_people = 10
+x = f"there are {types_of_people} types of people"
+binary = "binary"
+do_not = "dont"
+y = f"those who know {binary} and those who {do_not}."
+print(x)
+print(y)
+
+print(f"I said {x}")
+print(f"I also said {y}")
+
+hilarious = False
+joke_evaluation = "isnt that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))
+w = "this is the left side of "
+e = "a string with right side"
+print(w + e)
+
+print("mary had a little lamb")
+print("its fleece was white as {}.".format('snow'))
+print("and everywhere that mary went")
+print("." * 10)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+end = ' '
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+
+
