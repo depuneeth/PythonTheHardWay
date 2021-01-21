574e1f50de7b639ab4f53f8be732b92a343a6b19
Sharath <sharath@fashtag.info>
2021-01-22 01:12:46 +0530

    while loop

---
+++ excercise20.py
@@ -0,0 +1,14 @@
+i = 0
+numbers = []
+
+while i < 6:
+    #print(f"the number is: {i}")
+    numbers.append(i)
+
+    i = i+1
+    print(numbers)
+
+#print(numbers)
+
+for num in numbers:
+    print(num)
