a092076bc0cadd2fba061207cb50a697cbe1910f
Sharath <sharath@fashtag.info>
2021-02-19 12:24:32 +0530

    single linked1

---
+++ exercise36.py
@@ -0,0 +1,35 @@
+class SingleLinkedList(object):
+
+    def __init__(self):
+        self.begin = None
+        self.end = None
+
+    def push(self, obj):
+        """Appends a new value on the end of the list."""
+
+    def pop(self):
+        """Removes the last item and returns it."""
+
+    def shift(self, obj):
+        """Another name for push."""
+
+    def unshift(self):
+        """Removes the first item and returns it."""
+
+    def remove(self, obj):
+        """Finds a matching item and removes it from the list."""
+
+    def first(self):
+        """Returns a *reference* to the first item, does not remove."""
+
+    def last(self):
+        """Returns a reference to the last item, does not remove."""
+
+    def count(self):
+        """Counts the number of elements in the list."""
+
+    def get(self, index):
+        """Get the value at index."""
+
+    def dump(self, mark):
+        """Debugging function that dumps the contents of the list."""
