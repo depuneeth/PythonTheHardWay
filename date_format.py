7543a7cf2ecae04992116e64947cb9b049b1af8f
Sharath <sharath@fashtag.info>
2021-02-21 20:24:28 +0530

    samples

---
+++ date_format.py
@@ -0,0 +1,13 @@
+from datetime import date
+Today_date = date.today()
+print("current date: ",Today_date)
+
+#for x in range(1,1001):
+#    print(str(x))
+
+str1= 'Hello'
+str2= 'world'
+str3 = str1 + str2
+print(str3)
+
+
